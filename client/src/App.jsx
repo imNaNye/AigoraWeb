5053e73c5c399bb2cfa57e0cea012f277db9a870
imNaNye <hyunkyungj0730@gmail.com>
2025-11-22 01:59:34 +0900

    [chore] sibal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
