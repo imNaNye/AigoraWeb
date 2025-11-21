3a556ed6c08a4452ed5f09b4d3e08288b18862b5
imNaNye <hyunkyungj0730@gmail.com>
2025-11-22 02:03:45 +0900

    [chore] emiSibal

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/NavBar"
 import MacVideo from "../components/MacVideo"
 import InquiryLinkFooter from "../components/InquiryLinkFooter"
 import Improving from "../components/Improving"
